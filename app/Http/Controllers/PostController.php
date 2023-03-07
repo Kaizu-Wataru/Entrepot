048d599e2c10d85653d0587fee329bba7b1357bc
Kaizu-Wataru <blue.kanjihakase0@ezweb.ne.jp>
2023-03-07 19:18:31 +0900

    ブログ投稿詳細の表示

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts'=>$post]);
+        
+    }
+    public function show(Post $post)
+    {
+       return view('posts/show')->with(['posts'=>$post]);
+    }
+    //
+}
+?>
